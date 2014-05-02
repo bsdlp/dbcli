1d975e3b44f30dfb1f0427c2f41302b8d376fb3d
Jon Chen <bsd@voltaire.sh>
2014-05-02 13:57:38 -0400

    add list_workouts()

--- cli.py
+++ cli.py
@@ -16,3 +16,14 @@ def list_programs():
         table.add_row([program['id'], program['title'], program['image_url']])
     table.align = 'l'
     return print(table)
+
+
+def list_workouts():
+    table = PrettyTable(['title', 'trainer_name', 'workout_description',
+                         'program_ids', 'image_url'])
+    for workout in _get_thing('workouts'):
+        table.add_row([workout['title'], workout['trainer_name'],
+                       workout['workout_description'], workout['program_ids'],
+                       workout['image_url']])
+    table.align = 'l'
+    return print(table)
