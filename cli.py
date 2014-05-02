328ff0017c607481432f8f6ab5d7a80ea9942e46
Jon Chen <bsd@voltaire.sh>
2014-05-02 14:46:25 -0400

    add Program class

--- cli.py
+++ cli.py
@@ -6,9 +6,25 @@ import requests
 
 _ENDPOINT = 'http://dbios.herokuapp.com/'
 
+
+class Program(object):
+    def __init__(self, id, title, image_url):
+        self.id = id
+        self.title = title
+        self.image_url = image_url
+
+    def get_workouts(self):
+        payload = {'program_id': self.id}
+        r = requests.get(_ENDPOINT+'workouts', params=payload)
+        workouts = r.json()
+        for workout in workouts:
+            yield Workout([workout[i] for i in Workout.keys()])
+
+
 def _get_thing(thing_type):
     r = requests.get(_ENDPOINT+thing_type)
     return r.json()
+
 
 def list_things(thing_type):
     if thing_type == 'programs':
