8a851aacd5c64c40484c0f98b0179ffbbd57e386
Jon Chen <bsd@voltaire.sh>
2014-05-02 21:25:17 -0400

    clean up list_all

--- cli.py
+++ cli.py
@@ -6,17 +6,19 @@ from api import LeClient
 
 api = LeClient()
 
+_workout_jawns = ['title', 'trainer_name', 'workout_description',
+                  'program_ids', 'image_url']
+_program_jawns = ['id', 'title', 'image_url']
+
 
 def list_all(thing_type):
     if thing_type == 'programs':
-        _jawns = ['id', 'title', 'image_url']
+        _jawns = _program_jawns
     elif thing_type == 'workouts':
-        _jawns = ['title', 'trainer_name', 'workout_description',
-                  'program_ids', 'image_url']
-
+        _jawns = _workout_jawns
     table = PrettyTable(_jawns)
-    for program in api.request(path=thing_type):
-        table.add_row([program[i] for i in _jawns])
+    for item in api.request(path=thing_type):
+        table.add_row([item[i] for i in _jawns])
 
     table.align = 'l'
     return print(table)
