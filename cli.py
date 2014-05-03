30f5ca7ff0e5dd9649c91c16dbc8372cab813bc8
Jon Chen <bsd@voltaire.sh>
2014-05-03 01:24:56 -0400

    pep8

--- cli.py
+++ cli.py
@@ -24,30 +24,34 @@ def _list_trainer(trainer_name, programs=None, workouts=None):
     if workouts:
         return _trainer_workouts
 
-    _program_ids = { i for x in _trainer_workouts for i in x['program_ids']}
+    _program_ids = {i for x in _trainer_workouts for i in x['program_ids']}
     _programs = api.request(path='programs')
     _trainer_programs = filter(
         lambda x: x['id'] in _program_ids, _programs)
     return _trainer_programs
+
 
 def _search(keyword, search_type=None):
     """
     :rtype:filter workouts based on case-insensitive search for <keyword> in
     workouts or programs depending on search_type input.
     """
-    if search_type == None:
+    if search_type is None:
         search_type = ['programs', 'workouts']
 
     if 'programs' in search_type:
         _programs = api.request(path='programs')
         _search_programs = filter(
-            lambda x: search(keyword, ''.join(map(str, x.values())), IGNORECASE), _programs)
+            lambda x: search(
+                keyword, ''.join(map(str, x.values())), IGNORECASE), _programs)
         return _search_programs
     elif 'workouts' in search_type:
         _workouts = api.request(path='workouts')
         _search_workouts = filter(
-            lambda x: search(keyword, ''.join(map(str, x.values())), IGNORECASE), _workouts)
+            lambda x: search(
+                keyword, ''.join(map(str, x.values())), IGNORECASE), _workouts)
         return _search_workouts
+
 
 def tabulate(jawn_type):
     def wrapper(func):
@@ -60,6 +64,7 @@ def tabulate(jawn_type):
         return _wrapper
     return wrapper
 
+
 @tabulate(_workout_jawns)
 def list_workouts():
     """
@@ -68,6 +73,7 @@ def list_workouts():
     for item in api.request(path='workouts'):
         yield item
 
+
 @tabulate(_program_jawns)
 def list_programs():
     """
@@ -75,6 +81,7 @@ def list_programs():
     """
     for item in api.request(path='programs'):
         yield item
+
 
 @tabulate(_workout_jawns)
 def list_program_workouts(program_id=None, program_title=None):
@@ -94,17 +101,21 @@ def list_program_workouts(program_id=None, program_title=None):
         lambda x: _program['id'] in x['program_ids'], _workouts)
     return _prog_workouts
 
+
 @tabulate(_workout_jawns)
 def list_trainer_workouts(trainer_name):
     return _list_trainer(trainer_name, workouts=True)
+
 
 @tabulate(_program_jawns)
 def list_trainer_programs(trainer_name):
     return _list_trainer(trainer_name, programs=True)
 
+
 @tabulate(_workout_jawns)
 def search_workouts(keyword):
     return _search(keyword, search_type='workouts')
+
 
 @tabulate(_program_jawns)
 def search_programs(keyword):
